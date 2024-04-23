7ac378037060d3f777b7f6c35b6838288e4f7464
Dağhan Sever <daghansever@gmail.com>
2024-04-23 17:32:52 +0300

    Creating new activities and switching between activities
    Also starting a design document to finalize what is going to be the end product.

--- app/src/main/java/com/example/advancedprogrammingproject/Seasons.java
+++ app/src/main/java/com/example/advancedprogrammingproject/Seasons.java
@@ -1,6 +1,5 @@
 package com.example.advancedprogrammingproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,26 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Seasons extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_seasons);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        configureSeasonButton();
+
+        configureBackButton();
     }
-    public void configureSeasonButton(){
-        Button seasonButton = (Button) findViewById(R.id.seasonButton);
-        seasonButton.setOnClickListener(new View.OnClickListener() {
+
+    public void configureBackButton(){
+        Button backbutton = (Button) findViewById(R.id.backButton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Seasons.class));
+                finish();
             }
         });
     }
