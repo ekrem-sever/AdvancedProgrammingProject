7bf141dc415d59aede9e94f3c26f38e3cdebb915
Dağhan Sever <daghansever@gmail.com>
2024-04-26 13:14:17 +0300

    Example Activity is done
    With this, all of the designed main areas are outlined. All thats left is to finish the activities one by one

--- app/src/main/java/com/example/advancedprogrammingproject/MainActivity.java
+++ app/src/main/java/com/example/advancedprogrammingproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         });
         configureSeasonButton();
         configureLogOutButton();
+        configureActivityButtons();
     }
     public void configureSeasonButton(){
         Button seasonButton = (Button) findViewById(R.id.seasonButton);
@@ -46,4 +48,84 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void configureActivityButtons(){
+        Button activityButton1 = (Button) findViewById(R.id.activity1);
+        activityButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton2 = (Button) findViewById(R.id.activity2);
+        activityButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton3 = (Button) findViewById(R.id.activity3);
+        activityButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton4 = (Button) findViewById(R.id.activity4);
+        activityButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton5 = (Button) findViewById(R.id.activity5);
+        activityButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton6 = (Button) findViewById(R.id.activity6);
+        activityButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton7 = (Button) findViewById(R.id.activity7);
+        activityButton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton8 = (Button) findViewById(R.id.activity8);
+        activityButton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton9 = (Button) findViewById(R.id.activity9);
+        activityButton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton10 = (Button) findViewById(R.id.activity10);
+        activityButton10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+        Button activityButton11 = (Button) findViewById(R.id.activity11);
+        activityButton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+            }
+        });
+    }
+
 }
