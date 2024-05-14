9d4b4eb443b6a380ff3e5efba49fba25c25e165b
aerasis-com <ekremsever@gmail.com>
2024-05-14 11:36:44 +0300

    upd

--- app/src/main/java/com/example/advancedprogrammingproject/Log_In.java
+++ app/src/main/java/com/example/advancedprogrammingproject/Log_In.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +16,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import org.w3c.dom.Text;
 
 public class Log_In extends AppCompatActivity {
 
